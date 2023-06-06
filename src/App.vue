2bc242d37ddd713ea845ae902f6a7078c24f996b
Akuma95 <53189877+Akuma95@users.noreply.github.com>
2023-06-06 22:10:17 +0200

    first basic structure

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,7 @@
 <template>
   <img alt="Vue logo" src="./assets/logo.png">
   <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <router-view />
 </template>
 
 <script>
@@ -15,12 +16,19 @@ export default {
 </script>
 
 <style>
+html {
+  height: 100vh;
+  width: 100vw;
+}
+body {
+  background: radial-gradient(50% 50% at 50% 50%, #BB1A68 0%, #BB1A68 60.42%, #93134A 100%);
+  background-repeat: no-repeat;
+}
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
 }
 </style>
