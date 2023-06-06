53361a08489b3826aaa8674e8ab0772beb5af351
Akuma95 <53189877+Akuma95@users.noreply.github.com>
2023-06-06 22:11:13 +0200

    first basic structure

---
+++ src/views/home.vue
@@ -0,0 +1,30 @@
+<template>
+    <h1>PetersCore</h1>
+    <div class="left">
+        <p>left</p>
+    </div>
+    <div class="right">
+        <p>right</p>
+    </div>
+</template>
+  
+  <script>
+  import HelloWorld from './components/HelloWorld.vue'
+  
+  export default {
+    name: 'App',
+    components: {
+      HelloWorld
+    }
+  }
+  </script>
+  
+  <style scoped>
+  .left {
+    float: left;
+  }
+  .right {
+    float: right;
+  }
+  </style>
+  
